0f38ff284076e71f819c6ddadbe577c9afa05710
Algiane Froehly <algiane@gmail.com>
2022-04-11 14:25:06 +0200

    Remove useless openMPI dependency.

--- cmake/modules/optional-dependencies.cmake
+++ cmake/modules/optional-dependencies.cmake
@@ -232,21 +232,6 @@ If ( FXT_FOUND )
   SET( LIBRARIES ${FXT_LIBRARIES} ${LIBRARIES})
 ENDIF()
 
-############################################################################
-#####
-#####         OpenMP
-#####
-############################################################################
-
-## warning Useless I think: I don't think that we have a openMP dependency
-
-# Find OpenMP library?
-find_package(OpenMP QUIET)
-if (OPENMP_FOUND)
-    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
-endif()
 
 ############################################################################
 #####
